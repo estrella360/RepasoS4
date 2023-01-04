3b9b3cc2dac3af8af8a3cef2ed0f71fca2c016fe
master <master@SMLAB408PC12_23>
2023-01-03 19:08:11 -0500

    Agregar archivos de proyecto.

---
+++ RepasoS4/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using RepasoS4.Vistas;
+
+namespace RepasoS4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Inicio();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RepasoS4/Vistas/Registros.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RepasoS4.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registros : ContentPage
+    {
+        public Registros()
+        {
+            InitializeComponent();
+        }
+    }
+}
